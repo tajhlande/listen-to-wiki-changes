a9c4cc22b64d154b61d12d887999a99ba9666a5b
Tajh Taylor <ttaylor@wikimedia.org>
2025-05-03 11:18:32 +0300

    Refactored project structure to add Python API
    * Moved Vue app into web_app subdirectory
    * created Python API app in l2wc_api subdirectory
    * set up Python app with uv
    * added enough code to confirm FastAPI works

---
+++ l2wc_api/main.py
@@ -0,0 +1,23 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+def main():
+    print("Hello from l2wc-api!")
+
+
+
+if __name__ == "__main__":
+    main()
