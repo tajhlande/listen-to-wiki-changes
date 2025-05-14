55c03a8ff48846f3f6e5da660f902919a563d174
Tajh Taylor <ttaylor@wikimedia.org>
2025-05-14 11:19:06 -0400

    Adding npm to Procfile

--- web_app/vite.config.js
+++ web_app/vite.config.js
@@ -3,10 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'web_app', // Still uses web_app as root
   plugins: [vue()],
   build: {
     target: 'esnext', // this makes vue happy with top-level awaits,
-    outDir: 'dist' // Github Pages doesn't like dist but we're not using GHP anymore!
+    outDir: 'dist', // Github Pages doesn't like dist but we're not using GHP anymore!
+    emptyOutDir: true,
   },
-  base: '/app' // create relative paths when rendering
+  base: '/app', // create relative paths when rendering
+
 })
